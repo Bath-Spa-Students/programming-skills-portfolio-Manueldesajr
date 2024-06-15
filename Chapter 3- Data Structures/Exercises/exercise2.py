3f9c61bb5a04db8e0ff8cbf127dca64337b6d0af
Manueldesajr <162541970+Manueldesajr@users.noreply.github.com>
2024-06-16 00:49:54 +0400

    Create exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,11 @@
+'''Start with the list you used in Exercise 1, but instead of just
+printing each person`s name, print a message to them. The text of each message should be the same,\
+      but each message should be 
+personalized with the person`s name.'''
+
+# List of friends names
+names = ["Manuel", "Lua", "Eva", "Mary"]
+
+# Print personalized message for each name
+for name in names:
+    print(f"Salutations {name}!, Originally stolen by the doctor trusted to perform his autopsy, scientists over the decades have examined the brain of Albert Einstein to try and determine what made this seemingly normal man tick..")
