d862aac29cdcb2f1cd20f4e04c69f1da74915645
Manueldesajr <162541970+Manueldesajr@users.noreply.github.com>
2024-06-16 01:15:16 +0400

    Create exercise1.py

---
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -0,0 +1,10 @@
+'''Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter.\
+   Call the function, and make sure the message displays correctly.'''
+
+# Define a function names display_message()
+def display_message():
+    # Inside the function, print the message to display
+    print(" I am learning about Functions in this Chapter.")
+
+# Call the function 'display_message' to execute its code 
+display_message()
