941f7e652889c5b48f072ae2c8a1936c42cb416f
Manueldesajr <162541970+Manueldesajr@users.noreply.github.com>
2024-06-16 00:31:09 +0400

    Create exercise1.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise1.py
@@ -0,0 +1,11 @@
+'''Assign a message to a variable, and print that message.
+Then change the value of the variable to a new message, and print the new
+message.'''
+
+# Assigning a message to a variable
+message = "Hi, this is the first message!"
+print(message)
+
+# Changing the value of the variable
+message = "And this is the new message!"
+print(message)
